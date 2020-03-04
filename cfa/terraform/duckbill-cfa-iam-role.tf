94d6c9b607523e38e217bf13f8caeb0a727063a5
Matt Greensmith <matt.greensmith@gmail.com>
2020-03-04 15:03:58 -0800

    ViewOnlyAccess is a job-function policy.

--- cfa/terraform/duckbill-cfa-iam-role.tf
+++ cfa/terraform/duckbill-cfa-iam-role.tf
@@ -106,7 +106,7 @@ resource "aws_iam_policy" "DuckbillGroupCURIngestPipeline_policy" {
 
 resource "aws_iam_role_policy_attachment" "duckbill-attach-ViewOnlyAccess" {
   role       = "${aws_iam_role.DuckbillGroupRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/ViewOnlyAccess"
+  policy_arn = "arn:aws:iam::aws:policy/job-function/ViewOnlyAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupResourceDiscovery_policy" {
