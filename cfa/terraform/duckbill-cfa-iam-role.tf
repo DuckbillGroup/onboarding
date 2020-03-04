d9638ebe0504096dd493004255340d59ae3d41c7
Matt Greensmith <matt.greensmith@gmail.com>
2020-03-04 14:32:34 -0800

    Add ResourceDiscovery policy to CFA role, rename CFA role.

--- cfa/terraform/duckbill-cfa-iam-role.tf
+++ cfa/terraform/duckbill-cfa-iam-role.tf
@@ -36,8 +36,31 @@ data "aws_iam_policy_document" "DuckbillGroup_AssumeRole_policy_document" {
 }
 
 resource "aws_iam_role" "DuckbillGroupRole" {
-  name               = "DuckbillGroupRole"
+  name               = "DuckbillGroupRole-CFA"
   assume_role_policy = "${data.aws_iam_policy_document.DuckbillGroup_AssumeRole_policy_document.json}"
+}
+
+
+# DuckbillGroupResourceDiscovery IAM Policy
+
+data "aws_iam_policy_document" "DuckbillGroupResourceDiscovery_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "tagging:GetResources",
+      "s3:GetReplication*",
+      "s3:GetBucket*",
+      "es:ListTags"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "DuckbillGroupResourceDiscovery_policy" {
+  name   = "DuckbillGroupResourceDiscovery"
+  policy = "${data.aws_iam_policy_document.DuckbillGroupResourceDiscovery_policy_document.json}"
 }
 
 
@@ -81,12 +104,17 @@ resource "aws_iam_policy" "DuckbillGroupCURIngestPipeline_policy" {
 
 # Attach IAM Policies to DuckbillGroup Role
 
-resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupCURIngestPipeline_policy" {
-  role       = "${aws_iam_role.DuckbillGroupRole.name}"
-  policy_arn = "${aws_iam_policy.DuckbillGroupCURIngestPipeline_policy.arn}"
-}
-
 resource "aws_iam_role_policy_attachment" "duckbill-attach-ViewOnlyAccess" {
   role       = "${aws_iam_role.DuckbillGroupRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/ViewOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupResourceDiscovery_policy" {
+  role       = "${aws_iam_role.DuckbillGroupRole.name}"
+  policy_arn = "${aws_iam_policy.DuckbillGroupResourceDiscovery_policy.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupCURIngestPipeline_policy" {
+  role       = "${aws_iam_role.DuckbillGroupRole.name}"
+  policy_arn = "${aws_iam_policy.DuckbillGroupCURIngestPipeline_policy.arn}"
 }
