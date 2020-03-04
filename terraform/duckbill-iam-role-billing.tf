a380dceca183ce925b452a53371e62c1728e655b
Matt Greensmith <matt.greensmith@gmail.com>
2020-03-04 12:32:21 -0800

    Update COP TF to add CUR bucket access.

--- terraform/duckbill-iam-role-billing.tf
+++ terraform/duckbill-iam-role-billing.tf
@@ -1,9 +1,29 @@
-# https://www.terraform.io/downloads.html
+# These Terraform resrources create a remote access role for Duckbill Group
+# for a Cost Optimization Project.
+
+
+# Variables
+
+variable "customer_name_slug" {
+  type        = string
+  description = "A short, lower-case slug that identifies your company, e.g. 'acme-corp'. Duckbill Group will need to know this value, so that we can set up our own infrastructure for you."
+}
+
+variable "cur_bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket in which you are storing Cost and Usage Reports."
+}
+
+
+# Providers
+
 provider "aws" {
   region = "us-east-1"
 }
 
+
 # DuckbillGroup IAM Role
+
 data "aws_iam_policy_document" "DuckbillGroup_AssumeRole_policy_document" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -28,8 +48,10 @@ resource "aws_iam_role" "DuckbillGroupRole" {
   assume_role_policy = "${data.aws_iam_policy_document.DuckbillGroup_AssumeRole_policy_document.json}"
 }
 
-# DuckbillGroup IAM Policy
-data "aws_iam_policy_document" "DuckbillGroup_policy_document" {
+
+# DuckbillGroupBilling IAM Policy
+
+data "aws_iam_policy_document" "DuckbillGroupBilling_policy_document" {
   statement {
     effect = "Allow"
 
@@ -49,15 +71,60 @@ data "aws_iam_policy_document" "DuckbillGroup_policy_document" {
   }
 }
 
-resource "aws_iam_policy" "DuckbillGroup_policy" {
+resource "aws_iam_policy" "DuckbillGroupBilling_policy" {
   name   = "DuckbillGroupBilling"
-  policy = "${data.aws_iam_policy_document.DuckbillGroup_policy_document.json}"
+  policy = "${data.aws_iam_policy_document.DuckbillGroupBilling_policy_document.json}"
 }
 
+
+# DuckbillGroupCURIngestPipeline IAM Policy
+
+data "aws_iam_policy_document" "DuckbillGroupCURIngestPipeline_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.cur_bucket_name}",
+      "arn:aws:s3:::${var.cur_bucket_name}/*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+
+    resources = [
+      "arn:aws:s3:::dbg-cur-ingest-${var.customer_name_slug}",
+      "arn:aws:s3:::dbg-cur-ingest-${var.customer_name_slug}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "DuckbillGroupCURIngestPipeline_policy" {
+  name   = "DuckbillGroupCURIngestPipeline"
+  policy = "${data.aws_iam_policy_document.DuckbillGroupCURIngestPipeline_policy_document.json}"
+}
+
+
 # Attach IAM Policies to DuckbillGroup Role
-resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroup_policy" {
+
+resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupBilling_policy" {
   role       = "${aws_iam_role.DuckbillGroupRole.name}"
-  policy_arn = "${aws_iam_policy.DuckbillGroup_policy.arn}"
+  policy_arn = "${aws_iam_policy.DuckbillGroupBilling_policy.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupCURIngestPipeline_policy" {
+  role       = "${aws_iam_role.DuckbillGroupRole.name}"
+  policy_arn = "${aws_iam_policy.DuckbillGroupCURIngestPipeline_policy.arn}"
 }
 
 resource "aws_iam_role_policy_attachment" "duckbill-attach-Billing" {
