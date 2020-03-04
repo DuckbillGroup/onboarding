08217ed27a9553c9b490b1c753525777cbcb44f7
Matt Greensmith <matt.greensmith@gmail.com>
2020-03-04 14:02:35 -0800

    Remove CUR bucket access policies from COP role.

--- cop/terraform/duckbill-cop-iam-role.tf
+++ cop/terraform/duckbill-cop-iam-role.tf
@@ -2,19 +2,6 @@
 # for a Cost Optimization Project.
 
 
-# Variables
-
-variable "customer_name_slug" {
-  type        = string
-  description = "A short, lower-case slug that identifies your company, e.g. 'acme-corp'. Duckbill Group will need to know this value, so that we can set up our own infrastructure for you."
-}
-
-variable "cur_bucket_name" {
-  type        = string
-  description = "Name of the S3 bucket in which you are storing Cost and Usage Reports."
-}
-
-
 # Providers
 
 provider "aws" {
@@ -77,54 +64,11 @@ resource "aws_iam_policy" "DuckbillGroupBilling_policy" {
 }
 
 
-# DuckbillGroupCURIngestPipeline IAM Policy
-
-data "aws_iam_policy_document" "DuckbillGroupCURIngestPipeline_policy_document" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "s3:ListBucket",
-      "s3:GetObject"
-    ]
-
-    resources = [
-      "arn:aws:s3:::${var.cur_bucket_name}",
-      "arn:aws:s3:::${var.cur_bucket_name}/*"
-    ]
-  }
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "s3:ListBucket",
-      "s3:PutObject",
-      "s3:PutObjectAcl"
-    ]
-
-    resources = [
-      "arn:aws:s3:::dbg-cur-ingest-${var.customer_name_slug}",
-      "arn:aws:s3:::dbg-cur-ingest-${var.customer_name_slug}/*"
-    ]
-  }
-}
-
-resource "aws_iam_policy" "DuckbillGroupCURIngestPipeline_policy" {
-  name   = "DuckbillGroupCURIngestPipeline"
-  policy = "${data.aws_iam_policy_document.DuckbillGroupCURIngestPipeline_policy_document.json}"
-}
-
-
 # Attach IAM Policies to DuckbillGroup Role
 
 resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupBilling_policy" {
   role       = "${aws_iam_role.DuckbillGroupRole.name}"
   policy_arn = "${aws_iam_policy.DuckbillGroupBilling_policy.arn}"
-}
-
-resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupCURIngestPipeline_policy" {
-  role       = "${aws_iam_role.DuckbillGroupRole.name}"
-  policy_arn = "${aws_iam_policy.DuckbillGroupCURIngestPipeline_policy.arn}"
 }
 
 resource "aws_iam_role_policy_attachment" "duckbill-attach-Billing" {
