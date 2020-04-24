ccfbb7c064dcdaeac7b3d09660e465b4ecd909bc
Jesse DeRose <jesse@duckbillgroup.com>
2020-04-24 11:49:54 -0700

    Add ResourceDiscovery policy to COP role.

--- terraform/cop/duckbill-cop-iam-role.tf
+++ terraform/cop/duckbill-cop-iam-role.tf
@@ -56,6 +56,64 @@ resource "aws_iam_policy" "DuckbillGroupBilling_policy" {
   policy = "${data.aws_iam_policy_document.DuckbillGroupBilling_policy_document.json}"
 }
 
+# DuckbillGroupResourceDiscovery IAM Policy - a complimentary policy
+# to the actions allowed by the ViewOnlyAccess policy
+
+data "aws_iam_policy_document" "DuckbillGroupResourceDiscovery_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "acm:Describe*",
+      "apigateway:GET",
+      "batch:Describe*",
+      "cloudhsm:Describe*",
+      "cloudhsm:List*",
+      "cloudwatch:Describe*",
+      "codebuild:BatchGetProjects",
+      "codecommit:BatchGetRepositories",
+      "cognito-identity:Describe*",
+      "cognito-idp:Describe*",
+      "dax:Describe*",
+      "dlm:Get*",
+      "dms:Describe*",
+      "ec2:Describe*",
+      "eks:Describe*",
+      "eks:List*",
+      "elasticbeanstalk:ListTagsForResource",
+      "elasticfilesystem:Describe*",
+      "es:ListTags",
+      "fsx:Describe*",
+      "glue:Get*",
+      "health:Describe*",
+      "iam:GetRole",
+      "iam:GetUser",
+      "kafka:List*",
+      "kms:Describe*",
+      "kms:List*",
+      "lightsail:GetInstances",
+      "lightsail:GetLoadBalancers",
+      "lightsail:GetRelationalDatabases",
+      "mq:List*",
+      "redshift:Describe*",
+      "s3:GetBucket*",
+      "s3:GetReplication*",
+      "secretsmanager:ListSecrets",
+      "shield:List*",
+      "snowball:List*",
+      "sns:GetTopicAttributes",
+      "ssm:Describe*",
+      "tag:Get*"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "DuckbillGroupResourceDiscovery_policy" {
+  name   = "DuckbillGroupResourceDiscovery"
+  policy = "${data.aws_iam_policy_document.DuckbillGroupResourceDiscovery_policy_document.json}"
+}
 
 # Attach IAM Policies to DuckbillGroup Role
 
@@ -69,7 +127,12 @@ resource "aws_iam_role_policy_attachment" "duckbill-attach-Billing" {
   policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
 }
 
+resource "aws_iam_role_policy_attachment" "duckbill-attach-DuckbillGroupResourceDiscovery_policy" {
+  role       = "${aws_iam_role.DuckbillGroupRole.name}"
+  policy_arn = "${aws_iam_policy.DuckbillGroupResourceDiscovery_policy.arn}"
+}
+
 resource "aws_iam_role_policy_attachment" "duckbill-attach-ReadOnlyAccess" {
   role       = "${aws_iam_role.DuckbillGroupRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+  policy_arn = "arn:aws:iam::aws:policy/job-function/ViewOnlyAccess"
 }
